dd43320eb2ccaa118ab16e53f07808cbcf4c4d68
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-11-28 22:30:50 +0100

    Cleaned up onEnable method

--- src/main/java/com/zetaplugins/essentialz/EssentialZ.java
+++ src/main/java/com/zetaplugins/essentialz/EssentialZ.java
@@ -1,9 +1,5 @@
 package com.zetaplugins.essentialz;
 
-import com.zetaplugins.essentialz.features.EnchantmentManager;
-import com.zetaplugins.essentialz.features.GiveMaterialManager;
-import com.zetaplugins.essentialz.features.GodModeManager;
-import com.zetaplugins.essentialz.features.LastMsgManager;
 import com.zetaplugins.essentialz.features.economy.VaultEconomyImplProvider;
 import com.zetaplugins.essentialz.features.economy.manager.BuiltinEconomyManager;
 import com.zetaplugins.essentialz.features.economy.manager.EconomyManager;
@@ -31,6 +27,7 @@ public final class EssentialZ extends JavaPlugin {
     private static final List<String> ECONOMY_COMMANDS = List.of("balance", "pay", "baltop");
 
     private ConfigService configManager;
+    private ManagerRegistry managerRegistry;
 
     private final boolean hasPlaceholderApi = Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null;
     private final boolean hasVault = Bukkit.getPluginManager().getPlugin("Vault") != null;
@@ -43,18 +40,28 @@ public final class EssentialZ extends JavaPlugin {
         saveDefaultConfig();
         configManager = new ConfigService(this);
 
-        ManagerRegistry managerRegistry = new ManagerRegistry(this);
+        managerRegistry = new ManagerRegistry(this);
 
         managerRegistry.registerInstance(configManager);
         managerRegistry.registerInstance(Storage.class, createPlayerDataStorage());
         managerRegistry.registerInstance(new LanguageManager(this));
         managerRegistry.registerInstance(new MessageManager(this));
+        // Registry will automatically create and register other managers as needed
 
-        managerRegistry.registerInstance(new GiveMaterialManager(this));
-        managerRegistry.registerInstance(new GodModeManager());
-        managerRegistry.registerInstance(new LastMsgManager());
-        managerRegistry.registerInstance(new EnchantmentManager());
+        initEconomy();
 
+        registerCommands();
+        registerListeners();
+
+        getLogger().info("EssentialZ enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("EssentialZ disabled!");
+    }
+
+    private void initEconomy() {
         boolean economyEnabled = configManager.getConfig(EszConfig.ECONOMY).getBoolean("enabled", true);
         if (hasVault && economyEnabled) {
             getLogger().info("Vault detected, enabling Vault economy support.");
@@ -67,59 +74,53 @@ public final class EssentialZ extends JavaPlugin {
             getLogger().info("Economy system disabled.");
             managerRegistry.registerInstance(EconomyManager.class, new UnusedEconomyManager());
         }
+    }
 
+    private Storage createPlayerDataStorage() {
+        return switch (configManager.getConfig(EszConfig.STORAGE).getString("type").toLowerCase()) {
+            case "mysql" -> {
+                getLogger().info("Using MySQL storage");
+                yield new MySQLStorage(this);
+            }
+            case "sqlite" -> {
+                getLogger().info("Using SQLite storage");
+                yield new SQLiteStorage(this);
+            }
+            default -> {
+                getLogger().warning("Invalid storage type in config.yml! Using SQLite storage as fallback.");
+                yield new SQLiteStorage(this);
+            }
+        };
+    }
+
+    private void registerCommands() {
         List<String> registeredCommands = new AutoCommandRegistrar.Builder()
                 .setPlugin(this)
                 .setPackagePrefix(PACKAGE_PREFIX)
                 .setManagerRegistry(managerRegistry)
                 .build()
                 .registerAllCommands(this::shouldRegisterCommand);
-        writeCommandsInConfig(registeredCommands);
-        getLogger().info("Registered " + registeredCommands.size() + " commands.");
 
-        List<String> registeredEvents = new AutoEventRegistrar.Builder()
-                .setPlugin(this)
-                .setPackagePrefix(PACKAGE_PREFIX)
-                .setManagerRegistry(managerRegistry)
-                .build()
-                .registerAllListeners();
-        getLogger().info("Registered " + registeredEvents.size() + " event listeners.");
-
-        getLogger().info("EssentialZ enabled!");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("EssentialZ disabled!");
-    }
-
-    private Storage createPlayerDataStorage() {
-        switch (configManager.getConfig(EszConfig.STORAGE).getString("type").toLowerCase()) {
-            case "mysql":
-                getLogger().info("Using MySQL storage");
-                return new MySQLStorage(this);
-            case "sqlite":
-                getLogger().info("Using SQLite storage");
-                return new SQLiteStorage(this);
-            default:
-                getLogger().warning("Invalid storage type in config.yml! Using SQLite storage as fallback.");
-                return new SQLiteStorage(this);
-        }
-    }
-
-    /**
-     * Write the registered commands into the commands config file.
-     * @param commands The list of registered commands.
-     */
-    private void writeCommandsInConfig(List<String> commands) {
         FileConfiguration config = this.configManager.getConfig(EszConfig.COMMANDS);
-        commands.sort(String::compareTo);
-        for (String command : commands) {
+        registeredCommands.sort(String::compareTo);
+        for (String command : registeredCommands) {
             if (!config.contains(command)) {
                 config.set(command, true);
             }
         }
         this.configManager.saveConfig(EszConfig.COMMANDS, config);
+
+        getLogger().info("Registered " + registeredCommands.size() + " commands.");
+    }
+
+    private void registerListeners() {
+        List<String> registeredEventListeners = new AutoEventRegistrar.Builder()
+                .setPlugin(this)
+                .setPackagePrefix(PACKAGE_PREFIX)
+                .setManagerRegistry(managerRegistry)
+                .build()
+                .registerAllListeners();
+        getLogger().info("Registered " + registeredEventListeners.size() + " event listeners.");
     }
 
     /**
